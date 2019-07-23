0032dc0f5c03b670f17e4260161f34507701397b
Pasha Myndzul <51452458+PashaMyndzul@users.noreply.github.com>
2019-07-23 16:49:13 +0300

    Add files via upload

--- Timer/main.js
+++ Timer/main.js
@@ -1,63 +1,60 @@
-let time = 10;
-let time1 = 5940;
-
 function id(id) {
   return document.getElementById(id);
-};
-
-function Timer(obj) {
-  this.time = obj.time;
-  this.onTick = obj.onTick || null;
-  this.intervalValue = obj.intervalValue || null;
-  this.intervalUpdateTimer = obj.intervalUpdateTimer || null;
-  this.turnOn = obj.turnOn;
-  this.permission = obj.permission;
-  this.startTimer1 = () => {
-    if (this.turnOn == true && !this.permission) {
-      id(`button_Timer1`).innerText = `Stop`;
-      this.interval = setInterval(this.update, this.intervalValue);
-      this.permission = true;
-      this.turnOn = false;
-    } else if (!this.turnOn) {
-      id(`button_Timer1`).innerText = `Start`;
-      this.turnOn = true;
-      this.stop();
-      return this.permission = false;
-    }
-  }
-  this.startTimer2 = () => {
-    if (this.turnOn == true && !this.permission) {
-      id(`button_Timer2`).innerText = `Stop`;
-      this.interval = setInterval(this.update, this.intervalValue);
-      this.permission = true;
-      this.turnOn = false;
-    } else if (!this.turnOn) {
-      id(`button_Timer2`).innerText = `Start`;
-      this.turnOn = true;
-      this.stop();
-      return this.permission = false;
-    }
-  }
-  this.stop = () => {
-    clearInterval(this.interval);
-
-  }
-  this.update = () => {
-    this.time > 0 ? this.time -= this.intervalUpdateTimer : this.stop();
-    this.onTick ? this.onTick() : void 0;
-    return this.get();
-  };
-  this.get = () => {
-    return this.time;
-  };
 }
-
-function tick() {
-  id(`sec`).innerHTML = pad(timer1.get() % 60);
-  id(`min`).innerHTML = pad(parseInt(timer1.get() / 60));
-  id(`slider`).style.width = timer1.get() / time * 95 + "%";
-
-  function pad(val) {
+class Timer {
+  constructor(obj) {
+    this.time = obj.time || null;
+    this.onTick = obj.onTick || null;
+    this.intervalValue = obj.intervalValue || null;
+    this.intervalUpdateTimer = obj.intervalUpdateTimer || null;
+    this.turnOn = obj.turnOn;
+    this.permission = obj.permission;
+    this.domElemSec = obj.domElemSec;
+    this.domElemMin = obj.domElemMin;
+    this.buttonClick = obj.buttonClick;
+    this.line = obj.line;
+  }
+  startTimer() {
+    if (this.turnOn == true && !this.permission) {
+      id(`${this.buttonClick}`).innerText = `Stop`;
+      this.interval = setInterval(() => {
+        this.update()
+      }, this.intervalValue);
+      this.permission = true;
+      this.turnOn = false;
+    } else if (!this.turnOn) {
+      id(`${this.buttonClick}`).innerText = `Start`;
+      this.turnOn = true;
+      this.stop();
+      return this.permission = false;
+    }
+  }
+  stop() {
+    clearInterval(this.interval);
+  }
+  update() {
+    let endpoint = 0;
+    this.slider();
+    this.time > 0 ? this.time -= this.intervalUpdateTimer : this.stop();
+    if (this.time <= 0) {
+      return id(`${this.domElemSec}`).innerHTML = this.pad(endpoint);
+    }
+    this.tick ? this.tick() : void 0;
+    return this.time;
+  }
+  slider() {
+    let progress = document.querySelector(`.${this.line}`);
+    if (this.time <= this.intervalUpdateTimer) {
+      progress.style.width = 0;
+    }
+    let sliderTick = (this.time / this.intervalUpdateTimer);
+    let part = progress.clientWidth / sliderTick;
+    progress.style.width = progress.clientWidth - part + `px`;
+    if (this.time <= 0) {
+      progress.style.width = 0;
+    }
+  }
+  pad(val) {
     let valString = val + "";
     if (valString.length < 2) {
       return "0" + valString;
@@ -65,44 +62,40 @@ function tick() {
       return valString;
     }
   }
-};
-
-function tick1() {
-  id(`sec1`).innerHTML = pad(timer2.get() % 60);
-  id(`min1`).innerHTML = pad(parseInt(timer2.get() / 60));
-  id(`slider1`).style.width = timer2.get() / time1 * 95 + "%";
-
-  function pad(val) {
-    let valString = val + "";
-    if (valString.length < 2) {
-      return "0" + valString;
-    } else {
-      return valString;
-    }
+  tick() {
+    id(`${this.domElemSec}`).innerHTML = this.pad(this.time % 60);
+    id(`${this.domElemMin}`).innerHTML = this.pad(parseInt(this.time / 60));
   }
-};
+}
 const timer1 = new Timer({
-  time: time,
-  onTick: tick,
+  time: 3,
   intervalValue: 1000,
-  intervalUpdateTimer: 1,
-  turnOn: true,
-  permission: false
-});
-id(`button_Timer1`).addEventListener(`click`, () => {
-  timer1.startTimer1();
-});
-requestAnimationFrame(tick);
-const timer2 = new Timer({
-  time: time1,
-  onTick: tick1,
-  intervalValue: 2000,
   intervalUpdateTimer: 2,
   turnOn: true,
-  permission: false
+  permission: false,
+  domElemSec: `sec`,
+  domElemMin: `min`,
+  buttonClick: `button_Timer1`,
+  line: `slider1`
 });
-timer2.startTimer2();
+id(`button_Timer1`).addEventListener(`click`, () => {
+  timer1.startTimer();
+});
+const timer2 = new Timer({
+  time: 2,
+  intervalValue: 1000,
+  intervalUpdateTimer: 2,
+  turnOn: true,
+  permission: false,
+  domElemSec: `sec1`,
+  domElemMin: `min1`,
+  buttonClick: `button_Timer2`,
+  line: `slider2`
+});
+timer2.startTimer();
 id(`button_Timer2`).addEventListener(`click`, () => {
-  timer2.startTimer2();
+  timer2.startTimer();
+
 });
-requestAnimationFrame(tick1);
+timer1.tick();
+timer2.tick();
